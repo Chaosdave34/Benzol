7989fde59e5c8e9d74cb6e6c5f3dd6d37201455e
Chaosdave34 <david.emanuel@gmx.de>
2025-03-16 11:04:47 +0100

    Use cdn version of jspdf instead of node module

--- composeApp/src/wasmJsMain/kotlin/io/github/chaosdave34/benzol/files/htmlToPdf.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/io/github/chaosdave34/benzol/files/htmlToPdf.wasmJs.kt
@@ -1,17 +1,16 @@
 package io.github.chaosdave34.benzol.files
 
-@JsModule("html2pdf.js")
-external class html2pdf {
-    fun setMargin(src: Int): html2pdf
-    fun set(options: JsAny): html2pdf
-    fun from(src: String, type: String): html2pdf
-    fun toPdf(): html2pdf
+@JsName("html2pdf")
+external class Html2pdf {
+    fun setMargin(src: Int): Html2pdf
+    fun set(options: JsAny): Html2pdf
+    fun from(src: String, type: String): Html2pdf
+    fun toPdf(): Html2pdf
     fun save(filename: String)
 }
 
 external fun createOpt(scale: Int): JsAny
 
 actual fun htmlToPdf(html: String, directory: String, fileName: String) {
-    html2pdf().set(createOpt(4)).setMargin(16).from(html, "string").toPdf().save(fileName)
+    Html2pdf().set(createOpt(5)).setMargin(16).from(html, "string").toPdf().save(fileName)
 }
-
