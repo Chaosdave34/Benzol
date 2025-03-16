3b2bf55f57f6df5f6979f0b3ae52ebab2c479873
Chaosdave34 <david.emanuel@gmx.de>
2025-03-16 21:12:58 +0100

    Remove not required builder.useFastMode() as its enabled by default

--- composeApp/src/desktopMain/kotlin/io/github/chaosdave34/benzol/files/htmlToPdf.desktop.kt
+++ composeApp/src/desktopMain/kotlin/io/github/chaosdave34/benzol/files/htmlToPdf.desktop.kt
@@ -5,7 +5,6 @@ import java.io.FileOutputStream
 
 actual fun htmlToPdf(html: String, directory: String, fileName: String) {
     val builder = PdfRendererBuilder()
-    builder.useFastMode()
     builder.withHtmlContent(html, "")
     builder.toStream(FileOutputStream(directory + fileName))
     builder.run()
