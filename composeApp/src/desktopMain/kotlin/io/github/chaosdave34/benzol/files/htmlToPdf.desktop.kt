416e40ae31653e2137a0bd5719700442c0994da2
Chaosdave34 <david.emanuel@gmx.de>
2025-05-23 22:03:36 +0200

    Set logging for openhtmltopdf to warning

--- composeApp/src/desktopMain/kotlin/io/github/chaosdave34/benzol/files/htmlToPdf.desktop.kt
+++ composeApp/src/desktopMain/kotlin/io/github/chaosdave34/benzol/files/htmlToPdf.desktop.kt
@@ -1,9 +1,12 @@
 package io.github.chaosdave34.benzol.files
 
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
+import com.openhtmltopdf.util.XRLog
 import java.io.FileOutputStream
 
 actual fun htmlToPdf(html: String, directory: String, fileName: String) {
+    XRLog.listRegisteredLoggers().forEach { XRLog.setLevel(it, java.util.logging.Level.WARNING) }
+
     val builder = PdfRendererBuilder()
     builder.withHtmlContent(html, "")
     builder.toStream(FileOutputStream(directory + fileName))
